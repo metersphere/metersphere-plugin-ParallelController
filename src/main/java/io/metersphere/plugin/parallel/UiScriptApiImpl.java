bd499f6948492d87e0016acfb3e9017143139ef9
BugKing <wangzhen@fit2cloud.com>
2021-11-04 11:17:42 +0800

    feat: 初始化提交

---
+++ src/main/java/io/metersphere/plugin/parallel/UiScriptApiImpl.java
@@ -0,0 +1,54 @@
+package io.metersphere.plugin.parallel;
+
+import io.metersphere.plugin.core.api.UiScriptApi;
+import io.metersphere.plugin.core.ui.PluginResource;
+import io.metersphere.plugin.core.ui.UiScript;
+import io.metersphere.plugin.core.utils.LogUtil;
+
+import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+
+public class UiScriptApiImpl extends UiScriptApi  {
+    /**
+     * 企业版插件增加 这个方法
+     *
+     * @return 是否是企业版插件
+     */
+    public boolean xpack() {
+        return false;
+    }
+
+    @Override
+    public PluginResource init() {
+        LogUtil.info("开始初始化脚本内容 ");
+        List<UiScript> uiScripts = new LinkedList<>();
+        String scriptString = getJson("/json/ui_parallel.json");
+        UiScript script = new UiScript("parallel_controller", "并行控制器", "io.metersphere.plugin.parallel.controller.MsParallelController", scriptString);
+        script.setJmeterClazz("GenericController");
+
+        // 添加可选参数
+        script.setFormOption(getJson("/json/ui_form.json"));
+
+        uiScripts.add(script);
+        LogUtil.info("初始化脚本内容结束 ");
+        return new PluginResource("parallel-v1.0.0", uiScripts);
+    }
+
+    @Override
+    public String customMethod(String req) {
+        LogUtil.info("Parallel Controller 自定义方法");
+        return null;
+    }
+
+    public String getJson(String path) {
+        try {
+            InputStream in = UiScriptApiImpl.class.getResourceAsStream(path);
+            String json = org.apache.commons.io.IOUtils.toString(in);
+            return json;
+        } catch (Exception ex) {
+            LogUtil.error(ex.getMessage());
+        }
+        return null;
+    }
+}
---
+++ src/main/java/io/metersphere/plugin/parallel/controller/MsParallelController.java
@@ -0,0 +1,82 @@
+package io.metersphere.plugin.parallel.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.annotation.JSONField;
+import io.metersphere.plugin.core.MsParameter;
+import io.metersphere.plugin.core.MsTestElement;
+import io.metersphere.plugin.core.utils.LogUtil;
+import io.metersphere.plugin.parallel.utils.ElementUtil;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.jmeter.testelement.TestElement;
+import org.apache.jorphan.collections.HashTree;
+import com.blazemeter.jmeter.controller.ParallelSampler;
+
+import java.util.LinkedList;
+import java.util.List;
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class MsParallelController extends MsTestElement {
+    public MsParallelController() {
+
+    }
+    private String clazzName = "io.metersphere.plugin.parallel.controller.MsParallelController";
+
+    @JSONField(ordinal = 21)
+    private Boolean limitMaxThread;
+    @JSONField(ordinal = 22)
+    private String maxThreads;
+
+    @Override
+    public void toHashTree(HashTree tree, List<MsTestElement> hashTree, MsParameter config) {
+        LogUtil.info("===========开始转换MsDummySampler ==================");
+        if (!this.isEnable()) {
+            return;
+        }
+        ParallelSampler initParallelSampler = initParallelSampler();
+        if (initParallelSampler != null) {
+            final HashTree groupTree = tree.add(initParallelSampler());
+            if (CollectionUtils.isNotEmpty(hashTree)) {
+                hashTree.forEach(el -> {
+                    // 给所有孩子加一个父亲标志
+                    el.setParent(this);
+                    el.toHashTree(groupTree, el.getHashTree(), config);
+                });
+            }
+        }
+        else {
+            LogUtil.error("Connect Sampler 生成失败");
+            throw new RuntimeException("Connect Sampler生成失败");
+        }
+    }
+
+    public ParallelSampler initParallelSampler() {
+        try {
+            ParallelSampler parallelSampler = new ParallelSampler();
+            // base 执行时需要的参数
+            parallelSampler.setProperty("MS-ID", this.getId());
+            String indexPath = this.getIndex();
+            parallelSampler.setProperty("MS-RESOURCE-ID", this.getResourceId() + "_" + ElementUtil.getFullIndexPath(this.getParent(), indexPath));
+            List<String> id_names = new LinkedList<>();
+            ElementUtil.getScenarioSet(this, id_names);
+            parallelSampler.setProperty("MS-SCENARIO", JSON.toJSONString(id_names));
+
+            parallelSampler.setEnabled(this.isEnable());
+            parallelSampler.setName(this.getName());
+            parallelSampler.setProperty(TestElement.GUI_CLASS, "com.blazemeter.jmeter.controller.ParallelControllerGui");
+            parallelSampler.setProperty(TestElement.TEST_CLASS, "com.blazemeter.jmeter.controller.ParallelSampler");
+
+            parallelSampler.setProperty("PARENT_SAMPLE", false);
+            parallelSampler.setProperty("LIMIT_MAX_THREAD_NUMBER", this.getLimitMaxThread());
+            if (this.getLimitMaxThread() != null && this.getLimitMaxThread()) {
+                parallelSampler.setProperty("MAX_THREAD_NUMBER", this.getMaxThreads());
+            }
+
+            return parallelSampler;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
